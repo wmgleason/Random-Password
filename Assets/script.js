97e98088829cdbdd49a2046cbdf02f47f1997f60
Wanda Gleason <oglissane@gmail.com>
2020-12-30 21:39:19 -0500

    Added function to generate the password

--- Assets/script.js
+++ Assets/script.js
@@ -22,3 +22,23 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+
+function generatePassword(){
+  // WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+  var passwordLength = prompt("Please choose the number of characters for your password between 8 and 128");
+  // THEN I choose uppercase,
+  var whichCase = confirm("Do you want uppercase letters?");
+
+  var num = confirm("Do you want numbers in your password?");
+  
+  // This creates a local variable for the password instead of getting the DOM object with id="password"
+  let password = '';
+
+  console.log(passwordLength);
+  console.log(whichCase);
+  console.log(num);
