5f2c4b8be18d28acc3bfeb58c3d0ac02c2c5476e
Wanda Gleason <oglissane@gmail.com>
2020-12-30 19:52:40 -0500

    added variables to hold the different character options for the password

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,16 @@
 // Assignment Code
+// WHEN I click the button to generate a password
 var generateBtn = document.querySelector("#generate");
+var characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+                      "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
+var characterNum = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+                    "0","1","2","3","4","5","6","7","8","9"];
+
+var upperNum = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+                "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+                "0","1","2","3","4","5","6","7","8","9"];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
