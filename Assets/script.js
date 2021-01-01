42cd2ba36e559cc6bdfd9e6b20f9587f01ca954e
Wanda Gleason <oglissane@gmail.com>
2021-01-01 17:55:23 -0500

    Beginning new JS approach to solving Random Password Generator

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,37 @@
+// Second approach:
+// Assignment Code
+// WHEN I click the button to generate a password
+function generatePassword(passwordLength) {
+  var numberChars = "0123456789";
+  var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+  var specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~\"\"";
+  var allChars = numberChars + upperChars + lowerChars + specialChars;
+  var randPasswordArray = Array(passwordLength);
+  randPasswordArray[0] = numberChars;
+  randPasswordArray[1] = upperChars;
+  randPasswordArray[2] = lowerChars;
+  randPasswordArray = randPasswordArray.fill(allChars, 3);
+  return shuffleArray(randPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join('');
+}
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+alert(generatePassword(12));
+
+
+
+
+
+
 // Assignment Code
 // WHEN I click the button to generate a password
 var generateBtn = document.getElementById("generate");
